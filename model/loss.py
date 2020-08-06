676b16b860fe43595d2c1232270d5e401c77b8c4
sooooner <tnsgh0101@gmail.com>
2020-08-06 21:06:46 +0900

    add loss

---
+++ model/loss.py
@@ -0,0 +1,11 @@
+#-*- coding:utf-8 -*-
+import tensorflow as tf
+
+def Pairwise_ranking_loss(y_true, y_pred):
+    '''
+    ignore y_true
+    '''
+    positive_score = tf.keras.layers.Lambda(lambda x: x[:len(x)//2], output_shape= (1,))(y_pred)
+    negative_score = tf.keras.layers.Lambda(lambda x: x[len(x)//2:], output_shape= (1,))(y_pred)
+
+    return tf.keras.backend.mean(tf.math.maximum(0, 1 - positive_score + negative_score))
